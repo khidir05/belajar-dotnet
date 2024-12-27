d1d16165e5798e70e87c9418c62fac01c1e5304d
Khidir_Afwan_A <102141969+khidir05@users.noreply.github.com>
2024-12-27 22:53:52 +0700

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,8 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+Console.WriteLine("What is your Name");
+var name = Console.ReadLine();
+var CurrentDate = DateTime.Now;
+Console.WriteLine($"Hello {name}, today is {CurrentDate}");
+Console.Write($"{Environment.NewLine}Press any key to exit....");
+Console.ReadKey(true);
